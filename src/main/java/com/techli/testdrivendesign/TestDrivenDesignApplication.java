8c547a540e7d3e18d64d2a078eb8c648026e5077
Tegegn <tegegngelan@gmail.com>
2018-04-15 09:50:21 -0400

    Add files via upload

---
+++ src/main/java/com/techli/testdrivendesign/TestDrivenDesignApplication.java
@@ -0,0 +1,12 @@
+package com.techli.testdrivendesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDrivenDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDrivenDesignApplication.class, args);
+	}
+}
---
+++ src/test/java/com/techli/testdrivendesign/TestDrivenDesignApplicationTests.java
@@ -0,0 +1,16 @@
+package com.techli.testdrivendesign;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestDrivenDesignApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
